c809913283570d946edc91de5eab22192018bcd4
Cauan <cauangerlach297@gmail.com>
2024-04-11 14:59:26 -0300

    alterando o texto

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const mysql_config = require("./mysql_config");
 const app = express();
 
 app.listen(3000, () => {
-    console.log("Servdor na área mano!");
+    console.log("Servidor na área mano!");
 })
 
 const connection = mysql.createConnection(mysql_config);

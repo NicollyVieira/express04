0ae0d97a20f5ca12826521ef608097e3d30c43ba
Cauan <cauangerlach297@gmail.com>
2024-04-11 14:52:33 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+//criar a infraestrutura básica
+const express = require('express');
+const mysql = require('mysql2');
+
+const mysql_config = require("./mysql_config");
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log("Servdor na área mano!");
+})
+
+const connection = mysql.createConnection(mysql_config);
+
+// app.get('/',(req,res)=>{
+
+// })
